0c53142fc6cc1da84273b861f8edaf56b9f83892
sejfz <eliashh97@hotmail.com>
2019-03-01 01:15:02 +0100

    fixed accordding to comment

--- src/DetailView/DetailView.js
+++ src/DetailView/DetailView.js
@@ -16,10 +16,11 @@ class DetailView extends Component {
       dishId: "",
       numberOfGuests: modelInstance.getNumberOfGuests(),
       currentDishId: modelInstance.getCurrentId(),
-      displayedDish: [],
+      displayedDish: null,
       fullMenu: []
     };
     this.selectedDishObj = this.selectedDishObj.bind(this);
+    this.showallIngredients = this.showallIngredients.bind(this);
   }
 
   // this methods is called by React lifecycle when the
@@ -48,6 +49,35 @@ class DetailView extends Component {
     );
   }
 
+  showallIngredients() {
+    let ingredCount = 0;
+    let unsortedList = [];
+    let currentIngredients = this.state.displayedDish.extendedIngredients;
+    for (var ingred in currentIngredients) {
+      console.log(unsortedList);
+      unsortedList.push(
+        <li key={currentIngredients[ingred].id}>
+          {this.state.numberOfGuests * currentIngredients[ingred].amount +
+            " " +
+            currentIngredients[ingred].unit +
+            " " +
+            currentIngredients[ingred].name +
+            ", " +
+            this.state.numberOfGuests +
+            " SEK"}
+        </li>
+      );
+      ingredCount += 1;
+    }
+    unsortedList.push(
+      <strong key="totalPrice">
+        {"Total: " + this.state.numberOfGuests * ingredCount + " SEK"}
+      </strong>
+    );
+
+    return unsortedList;
+  }
+
   // this methods is called by React lifecycle when the
   // component is actually shown to the user (mounted to DOM)
   // that's a good place to call the API and get the data
@@ -58,9 +88,10 @@ class DetailView extends Component {
     modelInstance
       .getSpecificDish(this.state.currentDishId)
       .then(displayedDish => {
+        console.log(displayedDish);
         this.setState({
           status: "LOADED",
-          displayedDish: [displayedDish]
+          displayedDish: displayedDish
         });
       })
       .catch(() => {
@@ -74,81 +105,53 @@ class DetailView extends Component {
   }
 
   render() {
-    let dishesList = null;
-    let ingredCount = 0;
-    // depending on the state we either generate
-    // useful message to the user or show the list
-    // of returned displayedDish
+    console.log(this.state.displayedDish);
+    var ingredCount = 0;
     switch (this.state.status) {
       case "LOADING":
-        dishesList = <em>Loading...</em>;
-        break;
+        return <em>Loading...</em>;
       case "LOADED":
-        dishesList = this.state.displayedDish.map(dish => (
-          <div key={dish.id} className="row container">
-            <div id="displayView" className="col-sm-6">
-              <h3>{dish.title}</h3>
-              <img src={dish.image} />
-              <Link to="/search">
+        return (
+          <div className="col-sm-9">
+            <div key={this.state.displayedDish.id} className="row container">
+              <div id="displayView" className="col-sm-6">
+                <h3>{this.state.displayedDish.title}</h3>
+                <img src={this.state.displayedDish.image} />
+                <Link to="/search">
+                  <button
+                    id="backButton"
+                    type="button"
+                    className="btn btn-danger btn-sm"
+                  >
+                    Back to search
+                  </button>
+                </Link>
+                <h3>PREPARATION</h3>
+                <p>{this.state.displayedDish.instructions}</p>
+              </div>
+
+              <div id="ingredView" className="col-sm-3">
+                <p>
+                  <b>Ingredients for {this.state.numberOfGuests} people</b>
+                </p>
+                <ul>{this.showallIngredients()}</ul>
+
                 <button
-                  id="backButton"
+                  id="addToMenu"
                   type="button"
                   className="btn btn-danger btn-sm"
+                  onClick={this.selectedDishObj}
                 >
-                  Back to search
+                  Add to menu
                 </button>
-              </Link>
-              <h3>PREPARATION</h3>
-              <p>{dish.instructions}</p>
-            </div>
-
-            <div id="ingredView" className="col-sm-3">
-              <p>
-                <b>Ingredients for {this.state.numberOfGuests} people</b>
-              </p>
-              <ul>
-                {dish.extendedIngredients.map(
-                  ingred => (
-                    (ingredCount += 1),
-                    (
-                      <li key={ingred.id}>
-                        {" "}
-                        {this.state.numberOfGuests * ingred.amount +
-                          " " +
-                          ingred.unit +
-                          " " +
-                          ingred.name +
-                          " " +
-                          this.state.numberOfGuests +
-                          " SEK"}{" "}
-                      </li>
-                    )
-                  )
-                )}
-              </ul>
-              <p>
-                <strong>
-                  {"Total: " + this.state.numberOfGuests * ingredCount + " SEK"}
-                </strong>
-              </p>
-              <button
-                id="addToMenu"
-                type="button"
-                className="btn btn-danger btn-sm"
-                onClick={this.selectedDishObj}
-              >
-                Add to menu
-              </button>
+              </div>
             </div>
           </div>
-        ));
-        break;
-      default:
-        dishesList = <b>Failed to load data, please try again</b>;
-        break;
-    }
+        );
 
-    return <div className="currentDish col-sm-9">{dishesList}</div>;
+      default:
+        return <b>Failed to load data, please try again</b>;
+    }
   }
 }
 
