568795c750ea9aca0f7d734232cac9215bca9c2f
sejfz <eliashh97@hotmail.com>
2019-03-12 10:07:29 +0100

    Update FullRecipe.js

--- src/FullRecipe/FullRecipe.js
+++ src/FullRecipe/FullRecipe.js
@@ -4,36 +4,31 @@ import "./FullRecipe.css";
 import { Link } from "react-router-dom";
 
 class FullRecipe extends Component {
-    constructor(props) {
-      super(props);
-  
-      this.state = {
-        numberOfGuests: this.props.model.getNumberOfGuests(),
-        fullMenuList: this.props.model.getFullMenu(),
-        fullMenuPrice: this.props.model.getFullMenuPrice()
+  constructor(props) {
+    super(props);
 
-      };
-    }
+    this.state = {
+      numberOfGuests: this.props.model.getNumberOfGuests(),
+      fullMenuList: this.props.model.getFullMenu(),
+      fullMenuPrice: this.props.model.getFullMenuPrice()
+    };
+  }
 
-render() {
-    return(
-        <div className="container" align="center">
+  render() {
+    return (
+      <div className="container" align="center">
         <div className="row">
           <h1 className="col-12">
             {"My Dinner: " + this.state.numberOfGuests + " people"}
           </h1>
         </div>
-            <Link to="/confirmDinner">
-                <button 
-                    type="button" 
-                    class="btn btn-outline-danger col-3">
-                    Go back and edit dinner
-                </button>
-            </Link>
-        </div>
-
-
-        );
-    }
+        <Link to="/confirmDinner">
+          <button type="button" className="btn btn-outline-danger col-3">
+            Go back and edit dinner
+          </button>
+        </Link>
+      </div>
+    );
+  }
 }
 export default FullRecipe;
